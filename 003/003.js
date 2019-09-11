83ea7aef7d48b2e7f7e783f966e20b460e3328db
CodeMrZB <18838197439@163.com>
2019-09-12 00:19:35 +0800

    基础事件、事件对象

--- 003/003.js
+++ 003/003.js
@@ -519,11 +519,6 @@ $(function(){
     // $('window').bind('unload', function () {
     //     alert('当页面完全卸载后在windows上触发或当框架集卸载完毕后在框架集上触发');
     // });
-
-    $('window').resize(function(){
-        alert('1');
-    });
-
 });
 
 function fn() {
@@ -533,3 +528,395 @@ function fn() {
 function fn1() {
     alert('处理函数1');
 }
+
+$(function(){
+    // 当选择文本框的一个或多个字符时触发
+    // $('input').select(function(){
+    //     alert('选择文本');
+    // });
+
+    // 当文本框内容改变且失去焦点时触发
+    // $('input').change(function(){
+    //     alert('文本框内容改变');
+    // });
+
+    // 点击提交按钮时在form元素上触发
+    // $('form').submit(function () {
+    //     alert('点击提交按钮时在form元素上触发');
+    // });
+
+    // mouseover:鼠标移到元素上方时触发
+    // mouseout:鼠标从元素上方移除时触发
+    // $('div').mouseover(function(){
+    //     $(this).css('background', 'red');
+    // }).mouseout(function(){
+    //     $(this).css('background', 'green');
+    // });
+
+    // mouseenter、mouseleave:鼠标穿入穿出时触发
+    // $('div').mouseenter(function(){
+    //     $(this).css('background', 'red');
+    // }).mouseleave(function(){
+    //     $(this).css('background', 'green');
+    // });
+
+    // 鼠标移入的时候触发
+    // $('div').mouseover(function(){
+    //     $('strong').html(function(index, value){
+    //         return value + '1';
+    //     });
+    // });
+
+    // 鼠标移出的时候触发
+    // $('div').mouseout(function(){
+    //     $('strong').html(function(index, value){
+    //         return value + '1';
+    //     });
+    // });
+
+    // 鼠标穿入的时候触发
+    // $('div').mouseenter(function(){
+    //     $('strong').html(function(index, value){
+    //         return value + '1';
+    //     });
+    // });
+
+    // 鼠标穿出的时候触发
+    // $('div').mouseleave(function(){
+    //     $('strong').html(function(index, value){
+    //         return value + '1';
+    //     });
+    // });
+
+    // 按下键盘上任意键触发，如果按住不放会重复触发
+    // $('input').keydown(function(e){
+    //     alert(e.keyCode);
+    // });
+
+    // 按下键盘上字符键触发，如果按住不放会重复触发
+    // $('input').keypress(function(e){
+    //     alert(e.charCode);
+    // });
+
+    // 释放键盘上的任意键触发，如果按住不放会重复触发
+    // $('input').keyup(function(e){
+    //     alert(e.keyCode);
+    // });
+
+    // 光标激活(当页面或元素获得焦点时，在window及相关元素上触发)
+    // $('input').focus(function(){
+    //     alert('1');
+    // });
+
+    // 光标丢失(当页面或元素失去焦点时，在window及相关元素上触发)
+    // $('input').blur(function(){
+    //     alert('1');
+    // });
+
+    // focus:光标激活，blur:光标失去，操作对象必须是当前元素
+    // $('input').focus(function(){
+    //     alert('光标激活');
+    // });
+
+    // $('input').blur(function(){
+    //     alert('光标丢失');
+    // });
+
+    // focusin:光标激活，focusout：光标丢失，操作对象可以是当前元素，也可以是子元素
+    // $('div').focusin(function(){
+    //     alert('光标激活!');
+    // });
+
+    // $('div').focusout(function(){
+    //     alert('光标丢失!');
+    // });
+
+    // hover:鼠标移入的时候触发第一个函数，鼠标移出的时候触发第二个函数
+    // $('div').hover(function(){
+    //     $(this).css('background', 'green');
+    // }, function(){
+    //     $(this).css('background', 'chartreuse');
+    // });
+
+    // toggle:当鼠标点击触发第一个函数，再点击触发第二个函数，依次类推
+    // $('div').toggle('slow');
+
+    // $('div').toggle(function(){
+    //     $(this).css('background', 'red');
+    // }, function(){
+    //     $(this).css('background', 'green');
+    // }, function () {
+    //     $(this).css('background', 'blue');
+    // });
+
+    // var flag = 1;
+    // $('div').click(function(){
+    //     if (flag == 1) {
+    //         $(this).css('background', 'red');
+    //         flag = 2;
+    //     } else if (flag == 2) {
+    //         $(this).css('background', 'green');
+    //         flag = 3;
+    //     } else if (flag == 3) {
+    //         $(this).css('background', 'blue');
+    //         flag = 1;
+    //     }
+    // });
+});
+
+// 事件对象
+$(function(){
+    // 获取这个事件的事件类型
+    // $('input').bind('click', function(e){
+    //     alert(e.type);
+    // });
+
+    // 获取绑定事件的DOM元素
+    // $('div').bind('click', function(e){
+    //     alert(e.target);
+    // });
+
+    // 获取监听元素的DOM
+    // $('div').bind('click', function(e){
+    //     alert(e.currentTarget);
+    // });
+
+    // 获取移入移出目标点离开或进入的那个DOM元素
+    // $('div').bind('mouseover', function(e){
+    //     alert(e.relatedTarget);
+    // });
+
+    // $('span').bind('mouseout', function(e){
+    //     alert(e.relatedTarget);
+    // });
+
+    // currentTarget获取监听的元素
+    // $('div').bind('click', function(e){
+    //     alert(this == e.currentTarget);
+    // });
+
+    // target获取点击的元素
+    // $('div').bind('click', function(e){
+    //     alert(this == e.target);
+    // });
+
+    // data获取事件调用时的额外数据
+    // $('input').bind('click', 123, function(e){
+    //     alert(e.data);
+    // });
+
+    // $('input').bind('click', [1, 2, 3], function(e){
+    //     alert(e.data);
+    // });
+
+    // 获取事件调用时的额外数据
+    // $('input').bind('click', {'name':'张三', 'sex':'男', 'age':20}, function(e){
+    //     alert(e.data);
+    // });
+
+    // $('input').bind('click', 'abc', function(e){
+    //     alert(e.data);
+    // });
+
+    // 获取元素相对于页面原点的水平/垂直坐标
+    // $('div').bind('click', function(e){
+    //     alert('pageX:' + e.pageX + ', pageY:' + e.pageY);
+    // });
+
+    // 获取显示器屏幕位置的水平/垂直坐标
+    // $('div').bind('click', function(e){
+    //     alert('screenX:' + e.screenX + ',screenY:' + e.screenY);
+    // });
+
+    // 获取相对于页面视口的水平/垂直坐标
+    // $('div').bind('click', function(e){
+    //     alert('clientX:' + e.clientX + ',clientY:' + e.clientY);
+    // });
+
+    // 事件流描述的是从页面接受事件的顺序，当几个都具有事件的元素层叠在一起的时候，点击其中一个元素，并不是只有当前被点击的元素会被触发事件，而是层叠在点击范围的所有元素都会触发事件
+    // 事件流包括事件冒泡和事件捕获
+    // 事件冒泡:事件从里往外逐个触发
+    // 事件捕获:事件从外往里逐个触发
+    // $('input').bind('click', function(e){
+    //     alert('按钮被触发了!');
+    // });
+
+    // $('div').bind('click', function(e){
+    //     alert('Div被触发了!');
+    // });
+
+    // $(document).bind('click', function(e){
+    //     alert('document被触发了!')
+    // });
+
+    // $('input').click(function(e){
+    //     alert('按钮被触发了!');
+    // });
+
+    // $('div').click(function(e){
+    //     alert('div被触发了!')
+    // });
+
+    // $(document).click(function(e){
+    //     alert('document被点击了!');
+    // });
+
+    //  type获取这个事件的事件类型
+    // $('div').bind('click', function(e){
+    //     alert(e.type);
+    // });
+
+    // target获取点击的DOM元素
+    // $('div').bind('click', function(e){
+    //     alert(e.target);
+    // });
+
+    // currentTarget获取监听事件的DOM元素
+    // $('div').bind('click', function(e){
+    //     alert(e.currentTarget);
+    // });
+
+    // relatedTarget获取移入移出目标点离开或进入的那个DOM元素
+    // $('div').bind('mouseover', function(e){
+    //     alert(e.relatedTarget);
+    // });
+
+    // result获取上一个相同事件的返回值
+    // $('div').bind('click', function(e){
+    //     return 1243;
+    // });
+
+    // $('div').bind('click', function(e){
+    //     alert(e.result);
+    // });
+
+    // timeStamp获取事件触发的时间
+    // $('div').bind('click', function(e){
+    //     alert(e.timeStamp);
+    // });
+
+    // which获取鼠标左中右键(123)或获取键盘按键
+    // $('div').bind('mousedown', function(e){
+    //     alert(e.which);
+    // });
+
+    // $('input').bind('keydown', function(e){
+    //    alert(e.which);
+    // });
+
+    // altKey、shiftKey、ctrlKey、metaKey判断是否按下了alt键、shift键、ctrl键、meta键
+    // $('input').bind('keydown', function(e){
+    //     alert(e.shiftKey);
+    // });
+
+    // $('input').bind('keydown', function(e){
+    //     alert(e.ctrlKey);
+    // });
+
+    // $('input').bind('keydown', function(e){
+    //     alert(e.altKey);
+    // });
+
+    // $('input').bind('keydown', function(e){
+    //     alert(e.metaKey);
+    // });
+
+    // preventDefault()阻止事件的默认行为
+    // $('a').bind('click', function(e){
+    //     alert('百度');
+    //     e.preventDefault();
+    // });
+
+    // 禁止提交表单跳转
+    // $('input').bind('click', function(e){
+    //     e.preventDefault();
+    //     alert(e.type);
+    // });
+
+    // 禁止表单提交
+    // $('form').bind('submit', function(e){
+    //     alert('1');
+    //     e.preventDefault();
+    // });
+
+    // $('a').bind('click', function(e){
+    //     alert('a被触发了');
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    // });
+
+    // $('div').bind('click', function(e){
+    //     alert('Div被触发了');
+    // });
+
+    // $(document).bind('click', function(e){
+    //     alert('Document被触发了')
+    // });
+
+    // $('a').bind('click', function(e){
+    //     alert('a链接被触发了');
+    //     return false;
+    // });
+
+    // $('div').bind('click', function(e){
+    //     alert('div被触发了');
+    // });
+
+    // $(document).bind('click', function(e){
+    //     alert('document被触发了');
+    // });
+
+    // $('a').bind('click', function(e){
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    //     alert(e.isDefaultPrevented());
+    //     alert(e.isPropagationStopped());
+    //     alert('a');
+    // });
+
+    // $('div').bind('click', function(e){
+    //     alert('div');
+    // });
+
+    // $(document).bind('click', function(e){
+    //     alert('document');
+    // });
+
+    // $('a').bind('click', function (e) {
+    //     e.preventDefault();
+    //     e.stopPropagation();
+    //     alert('a');
+    // });
+
+    // $('a').bind('click', function (e) {
+    //     alert(e.isDefaultPrevented());
+    //     alert(e.isPropagationStopped());
+    // });
+
+    // $('div').bind('click', function (e) {
+    //     alert('div');
+    // });
+
+    // $(document).bind('click', function (e) {
+    //     alert('document');
+    // });
+
+    // 取消事件冒泡，并取消该事件的后续事件处理函数
+    // $('a').bind('click', function (e) {
+    //     e.stopImmediatePropagation();
+    //     alert(e.isImmediatePropagationStopped());
+    //     alert('a1');
+    // });
+
+    // $('a').bind('click', function (e) {
+    //     alert('a2');
+    // });
+
+    // $('div').bind('click', function(e){
+    //     alert('div');
+    // });
+
+    // $(document).bind('click', function (e) {
+    //     alert('document')
+    // });
+});
